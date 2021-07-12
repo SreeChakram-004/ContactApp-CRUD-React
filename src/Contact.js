abb221fbf1152b94f15ffb43fbf028ab49813d89
SreeChakram-004 <86343268+SreeChakram-004@users.noreply.github.com>
2021-07-12 17:51:24 +0530

    Initial commit

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h4> Sree Chakram</h4>
+      <h4> MERN Stack Developer</h4>
+      <h4> chakramsree@gmail.com</h4>
+    </div>
+  );
+};
+export default Contact;
